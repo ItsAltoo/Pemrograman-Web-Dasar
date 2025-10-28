87b61a4e66816410f5ac6ef6969ff6a756f38340
ItsAltoo <itsmalik57@gmail.com>
2025-10-28 10:04:31 +0800

    uts

---
+++ UTS/script.js
@@ -0,0 +1,8 @@
+const nameUser = document.getElementById("name").value;
+const nim = document.getElementById("nim").value;
+const programStudi = document.getElementById("program-studi").value;
+const email = document.getElementById("email").value;
+
+function onClick() {
+  console.log([nameUser, nim, programStudi, email]);
+}
