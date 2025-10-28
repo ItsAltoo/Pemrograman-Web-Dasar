381313fec96ab8e46dfebe572f9f1495d65fd08a
ItsAltoo <itsmalik57@gmail.com>
2025-10-28 16:44:43 +0800

    jgn di nilai, cmn ngebaikin code aja. Nilai yang di commit sebelumnya

--- UTS/script.js
+++ UTS/script.js
@@ -1,8 +1,18 @@
-const nameUser = document.getElementById("name").value;
-const nim = document.getElementById("nim").value;
-const programStudi = document.getElementById("program-studi").value;
-const email = document.getElementById("email").value;
+const nameUser = document.getElementById("name");
+const nim = document.getElementById("nim");
+const semester = document.getElementById("semester");
+const programStudi = document.getElementById("program-studi");
+const email = document.getElementById("email");
 
 function onClick() {
-  console.log([nameUser, nim, programStudi, email]);
+  const datas = {
+    name: nameUser.value,
+    nim: nim.value,
+    semester: semester.value,
+    programStudi: programStudi.value,
+    email: email.value,
+  };
+
+  console.log(datas);
+  alert("Data telah dikirim!");
 }
